d38bc3ad57a66a94b075f354dfa17a98197c6cc3
David Casas <cem20903@gmail.com>
2023-02-22 23:16:26 +0100

    Add tailwind config

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./public/**/*.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
+  theme: {
+    colors: {
+    'green': '#5EBEA3',
+    'grey-light': '#B5B5B5',
+    'grey-dark': '#3F3F3F',
+    'salmon': '#FF9984'
+    },
+    extend: {},
+  },
+  plugins: [],
+}
