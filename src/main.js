ac78f21e6a06464bfa8b2b42350ca124bcfa526b
David Casas <cem20903@gmail.com>
2023-02-24 07:56:11 +0100

    Update vue3

--- src/main.js
+++ src/main.js
@@ -1,9 +1,23 @@
-import Vue from 'vue'
+// import Vue from 'vue'
+import { createApp } from 'vue'
 import App from './App.vue'
 import './assets/tailwind.css'
+import { createStore } from 'vuex'
 
-Vue.config.productionTip = false
 
-new Vue({
-  render: h => h(App),
-}).$mount('#app')
+const store = createStore({
+  state () {
+    return {
+      count: 0
+    }
+  },
+  mutations: {
+  }
+})
+
+const app = createApp(App)
+
+app.use(store)
+
+
+app.mount('#app')
