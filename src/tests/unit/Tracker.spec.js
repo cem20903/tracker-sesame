c0fd67a7bc2f62ba2f161a84df01dbbee54f6500
David Casas <cem20903@gmail.com>
2023-02-25 16:58:13 +0100

    Add config tests and tracker test

---
+++ src/tests/unit/Tracker.spec.js
@@ -0,0 +1,41 @@
+import { render, fireEvent } from '@testing-library/vue'
+import App from '@/App.vue'
+
+import { workerOfflineResponse, workerOnlineResponse } from './workStatus.mock'
+import store from '@/store/store'
+import mockAxios from 'axios'
+
+describe('Tracker', () => {
+  
+  it('should call the correct endpoint when the user is online and click on Salir', async () => {
+    const data = { data: {
+      data: [workerOnlineResponse]
+    }} 
+    mockAxios.get.mockImplementationOnce(() => Promise.resolve(data))
+    
+    const { findByRole } = render(App, { global: {
+      plugins: [store]
+    }})
+    
+    const buttonExit = await findByRole('button', { name: 'Salir' })
+    fireEvent.click(buttonExit)
+  
+  
+    expect(mockAxios.post).toHaveBeenCalledWith("/clock-out", { employeeId: "b99a6cd9-3a3d-4635-9eea-e089c90ac45a", workEntryOut: { coordinates: {latitude: 39.46975, longitude: -0.37739}}})
+  })
+  
+  it('should call the correct endpoint when the user is offline and click on Entrar', async () => {
+    const data = { data: {
+      data: [workerOfflineResponse] }} 
+    mockAxios.get.mockImplementationOnce(() => Promise.resolve(data))
+    
+    const { findByRole } = render(App, { global: {
+      plugins: [store]
+    }})
+    
+    const buttonExit = await findByRole('button', { name: 'Entrar' })
+    fireEvent.click(buttonExit)
+  
+    expect(mockAxios.post).toHaveBeenCalledWith("/clock-in", { employeeId: "b99a6cd9-3a3d-4635-9eea-e089c90ac45a", workEntryIn: { coordinates: {latitude: 39.46975, longitude: -0.37739}}})
+  })
+})
---
+++ src/utils/__tests__/dates.spec.js
@@ -0,0 +1,5 @@
+
+
+describe('Timer utils', () => {
+
+})
